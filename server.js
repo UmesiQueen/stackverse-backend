17001a47642a749731f93c346aa68d25fc3a7b50
Queen Umesi <queenumesi01@gmail.com>
2025-07-15 07:24:11 +0400

    chore: update root route response to include available API routes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 import connectDB from './src/config/db.js';
 import lessonRoutes from './src/routes/lessons.js';
 import orderRoutes from './src/routes/orders.js';
-import {logger} from './src/middleware/logger.js';
-import {notFound} from './src/middleware/notFound.js';
+import { logger } from './src/middleware/logger.js';
+import { notFound } from './src/middleware/notFound.js';
 
 const app = Express();
 app.use(Express.json());
@@ -21,7 +21,15 @@ app.use(logger);
 // Routes
 app.use('/api/lessons', lessonRoutes);
 app.use('/api/orders', orderRoutes);
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(`    
+    <h1>Stackverse API</h1>
+    <h2>Available Routes</h2>
+    <ul>
+      <li>GET <a href="/api/lessons">/api/lessons</a> - List all lessons</li>
+      <li>POST <a href="/api/orders">/api/orders</a> - Create a new order</li>
+      <li>PUT <a href="/api/orders/update-availability">/api/orders/update-availability </a>- Update lesson availability after order</li>
+    </ul>
+  `));
 
 // Not Found Middleware
 app.use(notFound);
